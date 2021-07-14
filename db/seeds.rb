cd60cd36497aa0a4ffa6c8158aef339970a774c0
Terry <incubus8@gmail.com>
2021-07-13 22:06:26 -0400

    fixed code for unique pet_posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,45 +13,43 @@ end
 puts "Seeding pet posts..."
 
 # hard code 5-10 unique PetPosts
-    user = User.all.sample
-    likes = rand(1..1000)
     
-    PetPost.create(user_id: user.id,
+    PetPost.create(user_id: User.all.sample,
                     description: "So much floof!",
                     img_url: "https://i2-prod.mirror.co.uk/incoming/article22568810.ece/ALTERNATES/s1200b/0_Adorable-dog-leaves-people-baffled-as-its-face-looks-so-human-it-doesnt-seem-real.jpg",
-                    likes: likes,
+                    likes: rand(1..1000),
                     breed: "toy poodle",
                     name: "Kokoro"
                     )
 
-    PetPost.create(user_id: user.id,
+    PetPost.create(user_id: User.all.sample,
                     description: "Sorry, I'm not gluten free...",
                     img_url: "https://static.boredpanda.com/blog/wp-content/uploads/2020/06/funny-animals-stuck-in-bread-8-5ee76818ecb57__700.jpg",
-                    likes: likes,
+                    likes: rand(1..1000),
                     breed: "Pomeranian",
                     name: "Baguette"
                     )
 
-    PetPost.create(user_id: user.id,
+    PetPost.create(user_id: User.all.sample,
                     description: "I go sploot after a long day!",
                     img_url: "http://www.luvbat.com/uploads/funny_corgi_puppy_napping__5429175095.jpg",
-                    likes: likes,
+                    likes: rand(1..1000),
                     breed: "Corgi",
                     name: "Meatball"
                     )
 
-    PetPost.create(user_id: user.id,
+    PetPost.create(user_id: User.all.sample,
                     description: "I'm not a player, I just sag a lot!",
                     img_url: "https://static.boredpanda.com/blog/wp-content/uploads/2015/04/bulldog-puppy-17__605.jpg",
-                    likes: likes,
+                    likes: rand(1..1000),
                     breed: "English Bulldog",
                     name: "Sir Sags-A-Lot"
                     )
 
-    PetPost.create(user_id: user.id,
+    PetPost.create(user_id: User.all.sample,
                     description: "How do you like my smile?",
                     img_url: "https://justsomething.co/wp-content/uploads/2017/12/this-is-ryujii-the-handsome-and-ridiculously-cute-shiba-from-japan-01-12.jpg",
-                    likes: likes,
+                    likes: rand(1..1000),
                     breed: "Shiba Inu",
                     name: "Ryujii"
                     )
